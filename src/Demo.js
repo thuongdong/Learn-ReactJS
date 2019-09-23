6242767f7a4c1030c28111084b4e2e60bde72c06
Dong Duc Thuong <dongducthuong@gmail.com>
2019-09-23 09:56:13 +0700

    set port & start

---
+++ src/Demo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Demo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+export default Demo;
